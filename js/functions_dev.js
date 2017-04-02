0bd2ecaa9541cd1a37d35bd053454ac3a389c765
Jianfei Jiang <francisjjf9191@gmail.com>
2017-04-02 10:24:00 +0800

    Update functions_dev.js

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -128,7 +128,7 @@ function timeElapse(date) {
 		days = current.getDate() - date.getDate();
 		if (days < 0) {
 			days += getDaysInMonth(current.getMonth()-1);
-			current.setDate(current.getDate() - 1);
+			current.setMonth(current.setMonth() - 1);
 		}
 		months = current.getMonth() - date.getMonth();
 		if (months < 0) {
